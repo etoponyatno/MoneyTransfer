4a89d0d753b4eb7ca134df1e5cc8ffff902f7549
Petr Filatov <pmfilatov@gmail.com>
2020-09-14 09:58:50 +0300

    App and report added

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int current_count = 2_000_000_000;
+        int remittance = -2_100_000_000;
+        int total = current_count + remittance;
+        System.out.println(total);
+    }
+}
